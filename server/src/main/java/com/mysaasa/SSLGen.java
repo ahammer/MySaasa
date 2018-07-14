24a4db7759d85d170b2f481d7926746720960c46
Adam Hammer <adamhammer2@gmail.com>
2018-07-13 23:47:14 -0700

    Try and fix chain of trust

--- server/src/main/java/com/mysaasa/SSLGen.java
+++ server/src/main/java/com/mysaasa/SSLGen.java
@@ -47,7 +47,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class SSLGen {
 
 	static final String ROOT_KEY_URL = "https://letsencrypt.org/certs/isrgrootx1.pem.txt";
-	static final String INTERMEDIATE_KEY_URL = "https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt";
+	static final String INTERMEDIATE_KEY_URL = "https://letsencrypt.org/certs/letsencryptauthorityx3.pem.txt";
 	public static final int CERTIFICATE_LOOK_AHEAD_TIME_MS = 1000 * 60 * 60 * 24 * 14;
 	public static final String LETS_ENCRYPT_URL = "acme://letsencrypt.org/staging";
 	private KeyPair applicationKeyPair;
